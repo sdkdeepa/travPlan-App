97e5c10898d59e12253c8b295baf795f2a0cb574
sdkdeepa <deepa94086@gmail.com>
2021-03-13 14:17:38 -0800

    changed port
    Deploying for heroku

--- src/server/server.js
+++ src/server/server.js
@@ -18,8 +18,8 @@ app.get('/', (req, res) => {
 })
 module.exports = app;
 
-const port=3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`travPlan is listening to ${port}`);
+    console.log(`travPlan is listening to ${PORT}`);
   });
 
